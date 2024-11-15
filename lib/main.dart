6ac4988e3ea8a7a0e5902c7914e03354bde6c0cf
Ouchene Mohamed Nour El Islem <150658817+OucheneMohamedNourElIslem658@users.noreply.github.com>
2024-11-15 22:19:33 +0100

    session 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
@@ -24,10 +26,90 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
       ),
 
-      body: const Text("hello world"),
+      // body: const Center(
+      //   child: Text(
+      //     "Hello world",
+      //     style: TextStyle(
+      //       color: Color.fromARGB(100, 196, 7, 1),
+      //       fontSize: 50,
+      //       fontWeight: FontWeight.bold,
+      //     ),
+      //   ),
+      // ),
+
+      // body: Center(
+      //   child: Container(
+      //     height: 200,
+      //     width: 200,
+      //     decoration: BoxDecoration(
+      //       gradient: const LinearGradient(
+      //         begin: Alignment.topLeft,
+      //         end: Alignment.center,
+      //         colors: [
+      //           Colors.blue,
+      //           Colors.green
+      //         ]
+      //       ),
+      //       borderRadius: const BorderRadius.vertical(
+      //         bottom: Radius.circular(20)
+      //       ),
+      //       boxShadow: [
+      //         BoxShadow(
+      //           color: Colors.black.withOpacity(0.2),
+      //           offset: const Offset(10, 10),
+      //           spreadRadius: 20,
+      //           blurRadius: 20
+      //         ),
+      //       ]
+      //     ),
+      //   ),
+      // ),
+
+      // body: Center(
+      //   child: Container(
+      //     height: 200,
+      //     width: 200,
+      //     alignment: Alignment.center,
+      //     decoration: const BoxDecoration(
+      //       color: Colors.red,
+      //     ),
+      //     child: const Text(
+      //       "Hello world",
+      //       style: TextStyle(
+      //         fontSize: 30
+      //       ),
+      //     ),
+      //   ),
+      // ),
+
+      // body: const Center(
+      //   child: Icon(
+      //     // CupertinoIcons.home,
+      //     Icons.delete_forever_rounded,
+      //     size: 300,
+      //   ),
+      // ),
+
+      // body: Center(
+      //   child: Container(
+      //     height: 200,
+      //     width: 300,
+      //     color: Colors.red,
+      //     child: const Image(
+      //       image: NetworkImage("https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg"),
+      //       height: 300,
+      //       width: 300,
+      //       fit: BoxFit.cover,
+      //     ),
+      //   ),
+      // ),
+
+      body: Image(
+        image: AssetImage('images/person.jpg'),
+      ),
     );
   }
 }
